479a77a0b3090aabc13bbaf791c13b89cd22b56b
hyunho4532 <kyounguk192@gmail.com>
2023-12-05 23:04:33 +0900

    2023-12-03 BookReadActivity 구현 중

--- app/src/main/java/com/hyun/storyspotter/ui/book/read/BookReadActivity.kt
+++ app/src/main/java/com/hyun/storyspotter/ui/book/read/BookReadActivity.kt
@@ -2,14 +2,20 @@ package com.hyun.storyspotter.ui.book.read
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.hyun.storyspotter.R
 import com.hyun.storyspotter.databinding.ActivityBookReadBinding
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener
 
 class BookReadActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBookReadBinding
+
+    private val TAG = "BookReadActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +30,13 @@ class BookReadActivity : AppCompatActivity() {
 
         binding.tvReadBookTitle.text = title
 
+        binding.calendarview.setOnRangeSelectedListener(object: OnRangeSelectedListener {
+            override fun onRangeSelected(widget: MaterialCalendarView, dates: MutableList<CalendarDay>) {
+                val startDay = dates[0].date.toString();
+                val endDay = dates.get(dates.size - 1).date.toString()
 
+                Log.v(TAG, "시작일 : $startDay, 종료일 : $endDay")
+            }
+        })
     }
 }
