33a7f50d5a94b6825ca282646138c71011b738ce
hyunho4532 <kyounguk192@gmail.com>
2023-12-05 21:57:07 +0900

    2023-12-03 MaterialCalendarView 라이브러리 가져오기

--- app/src/main/java/com/hyun/storyspotter/ui/book/read/BookReadActivity.kt
+++ app/src/main/java/com/hyun/storyspotter/ui/book/read/BookReadActivity.kt
@@ -2,11 +2,28 @@ package com.hyun.storyspotter.ui.book.read
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.hyun.storyspotter.R
+import com.hyun.storyspotter.databinding.ActivityBookReadBinding
 
 class BookReadActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBookReadBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_book_read)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_book_read)
+
+        val imageUrl = intent.getStringExtra("imageUrl")
+        val title = intent.getStringExtra("title")
+
+        Glide.with(binding.root)
+            .load(imageUrl)
+            .into(binding.ivReadBook)
+
+        binding.tvReadBookTitle.text = title
+
+
     }
 }
